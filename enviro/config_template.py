e40e103c26ab2fef7ed7c21975024c5f0e68d7b2
Eduardo Gusmão <eduguscontra3@hotmail.com>
2025-10-22 16:03:15 +0100

    ha

--- enviro/config_template.py
+++ enviro/config_template.py
@@ -37,6 +37,9 @@ mqtt_broker_password = None
 # mqtt broker if using local SSL
 mqtt_broker_ca_file = None
 
+# Home Assistant Discovery setting
+hass_discovery = False # This could arguably be set to a text field for better customisability 
+
 # adafruit ui settings
 adafruit_io_username = None
 adafruit_io_key = None
